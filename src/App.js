952992e6ff9781fb0a9aa80c262656879be7443f
Marsel <marsel.kadroff@gmail.com>
2019-07-11 15:23:02 +0500

    save in localstorage

--- src/App.js
+++ src/App.js
@@ -4,16 +4,71 @@ import "./App.css";
 import styled from "styled-components";
 
 function App() {
-  const [values, setValues] = useState({ quantity: 1, price: 2, percent: 12 });
+  const [values, setValues] = useState({
+    quantity: 1,
+    price: 2,
+    percent: 12,
+    company_info: "www.example.com↵info@example.com",
+    company_name: "Example Co.",
+    description_label: "Item & Description",
+    invoice_date_label: "Date",
+    invoice_number: "1",
+    invoice_number_label: "Invoice #",
+    kind: "INVOICE",
+    notes_b: "↵Created with Billable.me",
+    price_label: "Price",
+    quantity_label: "Quantity",
+    recipient_info:
+      "Michael Scott Paper Company Inc.↵1725 Slough Avenue↵Scranton, Pennsylvania",
+    subtotal_label: "Subtotal",
+    tax_name: "VAT",
+    tax_percentage: "14",
+    total_label: "Total",
+    invoice_date: "10 July, 2019",
+    description_table_row: ""
+  });
   const ref = React.createRef();
   const subtotal = values.quantity * values.price;
   const percent = (values.percent / 100) * subtotal;
   const total = subtotal + percent;
+  const items = JSON.parse(localStorage.getItem("billable_items"));
+  const billableItems = JSON.parse(localStorage.getItem("billable"));
 
   const handleInputChange = e => {
     const value = e.target.value;
     const name = e.target.name;
     setValues({ ...values, [name]: value });
+  };
+
+  const setLocalStorage = () => {
+    const valueItems = {
+      quantity: values.quantity,
+      price: values.price
+    };
+    localStorage.setItem("billable_items", JSON.stringify(valueItems));
+    JSON.parse(localStorage.getItem("billable_items"));
+
+    const billableValue = {
+      company_info: values.company_info,
+      company_name: values.company_name,
+      description_label: values.description_label,
+      invoice_date_label: values.invoice_date_label,
+      invoice_number: values.invoice_number,
+      invoice_number_label: values.invoice_number_label,
+      kind: values.kind,
+      notes_b: values.notes_b,
+      price_label: values.price_label,
+      quantity_label: values.quantity_label,
+      recipient_info: values.recipient_info,
+      subtotal_label: values.subtotal_label,
+      tax_name: values.tax_name,
+      total_label: values.total_label,
+      invoice_date: values.invoice_date,
+      description_table_row: values.description_label
+    };
+
+    localStorage.setItem("billable", JSON.stringify(billableValue));
+    JSON.parse(localStorage.getItem("billable"));
   };
 
   return (
@@ -25,35 +80,97 @@ function App() {
         )}
       </Pdf>
 
+      <OptionButton type="button" value="Save" onClick={setLocalStorage} />
+
       <Pages ref={ref}>
-        <Kind type="text" defaultValue="INVOICE" />
-        <CompanyName defaultValue="Example Co." />
+        <Kind
+          type="text"
+          defaultValue={
+            billableItems != null ? billableItems.kind : values.kind
+          }
+          name="kind"
+          onChange={handleInputChange}
+        />
+        <CompanyName
+          name="company_name"
+          onChange={handleInputChange}
+          defaultValue={
+            billableItems != null
+              ? billableItems.company_name
+              : values.company_name
+          }
+        />
         <AddressCompany
-          defaultValue="www.example.com
-          info@example.com"
+          defaultValue={
+            billableItems != null
+              ? billableItems.company_info
+              : values.company_info
+          }
+          name="company_info"
+          onChange={handleInputChange}
         />
         <InformationCompany
-          defaultValue="Michael Scott Paper Company Inc.
-          1725 Slough Avenue
-          Scranton, Pennsylvania"
+          defaultValue={
+            billableItems != null
+              ? billableItems.recipient_info
+              : values.recipient_info
+          }
+          name="recipient_info"
+          onChange={handleInputChange}
         />
         <TableDate>
           <tbody>
             <tr>
               <th>
-                <TableDateHeader type="text" defaultValue="Invoice #" />
+                <TableDateHeader
+                  type="text"
+                  name="invoice_number_label"
+                  onChange={handleInputChange}
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.invoice_number_label
+                      : values.invoice_number_label
+                  }
+                />
               </th>
               <td>
-                <TableDateData type="text" defaultValue="1" />
+                <TableDateData
+                  type="text"
+                  name="invoice_number"
+                  onChange={handleInputChange}
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.invoice_number
+                      : values.invoice_number
+                  }
+                />
               </td>
             </tr>
 
             <tr>
               <th>
-                <TableDateHeader type="text" defaultValue="Date" />
+                <TableDateHeader
+                  type="text"
+                  name="invoice_date_label"
+                  onChange={handleInputChange}
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.invoice_date_label
+                      : values.invoice_date_label
+                  }
+                />
               </th>
               <td>
-                <TableDateData type="text" defaultValue="10 July, 2019" />
+                <TableDateData
+                  type="text"
+                  name="invoice_date"
+                  onChange={handleInputChange}
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.invoice_date
+                      : values.invoice_date
+                  }
+                />
               </td>
             </tr>
           </tbody>
@@ -64,26 +181,60 @@ function App() {
               <th style={{ width: "50%", backgroundColor: "#f1f4f3" }}>
                 <TableDateHeader
                   type="text"
-                  defaultValue="Item & Description"
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.description_label
+                      : values.description_label
+                  }
+                  name="description_label"
+                  onChange={handleInputChange}
                   style={{ width: "100%" }}
                 />
               </th>
               <th style={{ backgroundColor: "#f1f4f3" }}>
-                <TableDateHeader type="text" defaultValue="Quantity" />
+                <TableDateHeader
+                  type="text"
+                  name="quantity_label"
+                  onChange={handleInputChange}
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.quantity_label
+                      : values.quantity_label
+                  }
+                />
               </th>
               <th style={{ backgroundColor: "#f1f4f3" }}>
-                <TableDateHeader type="text" defaultValue="Price" />
+                <TableDateHeader
+                  type="text"
+                  name="price_label"
+                  onChange={handleInputChange}
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.price_label
+                      : values.price_label
+                  }
+                />
               </th>
             </tr>
             <tr>
-              <td className="description_table_row" style={{ width: "100%" }}>
+              <td
+                className="description_table_row"
+                name="description_table_row"
+                defaultValue={
+                  billableItems != null
+                    ? billableItems.description_table_row
+                    : values.description_table_row
+                }
+                onChange={handleInputChange}
+                style={{ width: "100%" }}
+              >
                 <TableDateData style={{ width: "100%", height: "100%" }} />
               </td>
               <td className="description_table_row">
                 <TableDateData
                   type="number"
                   name="quantity"
-                  value={values.quantity}
+                  defaultValue={items != null ? items.quantity : items.quantity}
                   onChange={handleInputChange}
                 />
               </td>
@@ -91,7 +242,7 @@ function App() {
                 <TableDateData
                   type="number"
                   name="price"
-                  value={values.price}
+                  defaultValue={items != null ? items.price : items.price}
                   onChange={handleInputChange}
                 />
               </td>
@@ -105,8 +256,14 @@ function App() {
               <th className="total_table_header">
                 <TableDateHeader
                   type="text"
-                  defaultValue="Subtotal"
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.subtotal_label
+                      : values.subtotal_label
+                  }
                   className="total_table"
+                  name="subtotal_label"
+                  onChange={handleInputChange}
                 />
               </th>
               <th className="total_table_percent" />
@@ -117,7 +274,13 @@ function App() {
               <th>
                 <TableDateHeader
                   type="text"
-                  defaultValue="VAT"
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.tax_name
+                      : values.tax_name
+                  }
+                  name="tax_name"
+                  onChange={handleInputChange}
                   className="total_table"
                 />
               </th>
@@ -125,7 +288,11 @@ function App() {
                 <TableDateData
                   name="percent"
                   type="number"
-                  value={values.percent}
+                  value={
+                    billableItems != null
+                      ? billableItems.percent
+                      : values.percent
+                  }
                   onChange={handleInputChange}
                   className="total_table"
                 />
@@ -137,7 +304,13 @@ function App() {
               <th>
                 <TableDateHeader
                   type="text"
-                  defaultValue="Total"
+                  defaultValue={
+                    billableItems != null
+                      ? billableItems.total_label
+                      : values.total_label
+                  }
+                  name="total_label"
+                  onChange={handleInputChange}
                   className="total_table"
                 />
               </th>
@@ -152,7 +325,11 @@ function App() {
         <FooterNotes />
         <FooterNotes
           style={{ textAlign: "right" }}
-          defaultValue="Created with Billable.me"
+          name="notes_b"
+          onChange={handleInputChange}
+          defaultValue={
+            billableItems != null ? billableItems.notes_b : values.notes_b
+          }
         />
       </Pages>
     </Main>
